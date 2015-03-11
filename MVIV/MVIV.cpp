5d1dbccbb15f5ee506bd68afc7ae3326142046c4
li-chi <lichi321@gmail.com>
2015-03-11 21:18:55 +0800

    change MVIV to random, check if varDecl hasInit in MVIV

--- MVIV/MVIV.cpp
+++ MVIV/MVIV.cpp
@@ -13,7 +13,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "clang/Lex/lexer.h"
 #include "clang/Lex/Token.h"
-
+#include <stdlib.h>  
 
 using namespace clang;
 using namespace clang::ast_matchers;
@@ -22,8 +22,10 @@ using namespace clang::tooling;
 
 static llvm::cl::OptionCategory MatcherSampleCategory("Matcher Sample");
 
-std::string varName;
-
+//std::string varName;
+bool counting = true;
+int count = 0;
+int choose;
 
 class VarDeclHandler : public MatchFinder::MatchCallback {
 public:
@@ -31,15 +33,29 @@ public:
 
     virtual void run(const MatchFinder::MatchResult &Result) {
       const VarDecl *vd = Result.Nodes.getNodeAs<clang::VarDecl>("vd");
+      
+      if(!vd->hasInit())
+        return;
+
+      if(counting) {
+        count++;
+        return;
+      } else {
+        count++;
+        if(count != choose) {
+          return;
+        }
+      }
+
       SourceLocation sl = vd->getSourceRange().getBegin();
-        
-        SourceLocation des = sl;
-        while (Lexer::findLocationAfterToken(des, tok::semi, Rewrite.getSourceMgr(), 
+    
+      SourceLocation des = sl;
+      while (Lexer::findLocationAfterToken(des, tok::semi, Rewrite.getSourceMgr(), 
                                     Rewrite.getLangOpts(), true).isInvalid()) {
           //des = Lexer::getLocForEndOfToken(sl, 0, Rewrite.getSourceMgr(), Rewrite.getLangOpts());
           des = des.getLocWithOffset(-1);
-        }
-        des = Lexer::findLocationAfterToken(des, tok::semi, Rewrite.getSourceMgr(), 
+      }
+      des = Lexer::findLocationAfterToken(des, tok::semi, Rewrite.getSourceMgr(), 
                                     Rewrite.getLangOpts(), true); 
       std::string str = vd->getType().getAsString() + " " + vd->getNameAsString() + "; // init var \n"
         + "// Missing init var " + vd->getNameAsString() + ": ";
@@ -62,7 +78,7 @@ public:
     MyASTConsumer(Rewriter &R) : VD(R) {
 
     
-        Matcher.addMatcher(varDecl(hasName(varName)).bind("vd"),&VD);
+        Matcher.addMatcher(varDecl().bind("vd"),&VD);
         //ignoringImpCasts(declRefExpr(to(functionDecl().bind("fun2"))));
     }
     
@@ -82,6 +98,7 @@ class MyFrontendAction : public ASTFrontendAction {
 public:
     MyFrontendAction() {}
     void EndSourceFileAction() override {
+        if(counting) return;
         TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID())
         .write(llvm::outs());
     }
@@ -97,10 +114,18 @@ private:
 };
 
 int main(int argc, const char **argv) {
+    /*
     std::cout << "Enter variable name: " << std::endl;
     std::getline (std::cin,varName);
+    */
     CommonOptionsParser op(argc, argv, MatcherSampleCategory);
     ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+    Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
     
+    counting = false;
+    srand(time(NULL));
+    choose = rand() % count + 1;
+    count = 0;
+
     return Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
 }
