155af616e4009287f8701b69b7f6ab4e9fe69a62
li-chi <lichi321@gmail.com>
2015-03-18 21:43:44 +0800

    add MLPA

---
+++ MLC/MLC.cpp
@@ -0,0 +1,127 @@
+
+#include <string>
+#include <iostream>
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/Support/raw_ostream.h"
+#include "clang/Lex/lexer.h"
+#include "clang/Lex/Token.h"
+#include <stdlib.h>  
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::driver;
+using namespace clang::tooling;
+
+static llvm::cl::OptionCategory MatcherSampleCategory("Matcher Sample");
+
+bool counting = true;
+int count = 0;
+int choose;
+
+class IfHandler : public MatchFinder::MatchCallback {
+public:
+    IfHandler(Rewriter &Rewrite) : Rewrite(Rewrite) {}
+
+    virtual void run(const MatchFinder::MatchResult &Result) {
+      const IfStmt *ifStmt = Result.Nodes.getNodeAs<IfStmt>("if");
+
+      if(ifStmt->getElse()) return;
+      else {
+        if(counting) {
+          count++;
+          return;
+        } else {
+          count++;
+          if(count != choose) {
+            return;
+          }
+        }
+
+        SourceLocation sl = ifStmt->getLocStart();
+        std::string str = "/* missing if construct:\n";
+        Rewrite.InsertTextAfter(sl, str);
+        const Expr *cond = ifStmt->getCond();
+        sl = cond->getLocEnd();
+        SourceLocation des = sl;
+        while (Lexer::findLocationAfterToken(des, tok::r_paren, Rewrite.getSourceMgr(), 
+                                    Rewrite.getLangOpts(), true).isInvalid()) {
+          //des = Lexer::getLocForEndOfToken(sl, 0, Rewrite.getSourceMgr(), Rewrite.getLangOpts());
+          des = des.getLocWithOffset(-1);
+        }
+        des = Lexer::findLocationAfterToken(des, tok::r_paren, Rewrite.getSourceMgr(), 
+                                    Rewrite.getLangOpts(), true); 
+        str = " end */";
+        Rewrite.InsertTextAfter(des, str);
+      }
+      
+    }
+    
+private:
+    Rewriter &Rewrite;
+};
+
+// Implementation of the ASTConsumer interface for reading an AST produced
+// by the Clang parser. It registers a couple of matchers and runs them on
+// the AST.
+class MyASTConsumer : public ASTConsumer {
+public:
+    MyASTConsumer(Rewriter &R) : Handler(R) {
+
+        
+        Matcher.addMatcher(ifStmt(hasCondition(binaryOperator(
+                                                               hasOperatorName("&&")))).bind("if"),&Handler);
+        //ignoringImpCasts(declRefExpr(to(functionDecl().bind("fun2"))));
+    }
+    
+    void HandleTranslationUnit(ASTContext &Context) override {
+        // Run the matchers when we have the whole TU parsed.
+        Matcher.matchAST(Context);
+    }
+    
+private:
+    MatchFinder Matcher;
+    IfHandler Handler;
+
+};
+
+// For each source file provided to the tool, a new FrontendAction is created.
+class MyFrontendAction : public ASTFrontendAction {
+public:
+    MyFrontendAction() {}
+    void EndSourceFileAction() override {
+        if(counting) return;
+        TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID())
+        .write(llvm::outs());
+    }
+    
+    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                   StringRef file) override {
+        TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+        return llvm::make_unique<MyASTConsumer>(TheRewriter);
+    }
+    
+private:
+    Rewriter TheRewriter;
+};
+
+int main(int argc, const char **argv) {
+    CommonOptionsParser op(argc, argv, MatcherSampleCategory);
+    ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+    
+    Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
+    
+    counting = false;
+    srand(time(NULL));
+    choose = rand() % count + 1;
+    count = 0;
+
+    return Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
+}
---
+++ MLPA/MLPA.cpp
@@ -0,0 +1,126 @@
+
+#include <string>
+#include <iostream>
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/Support/raw_ostream.h"
+#include "clang/Lex/lexer.h"
+#include "clang/Lex/Token.h"
+#include <stdlib.h>  
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::driver;
+using namespace clang::tooling;
+
+static llvm::cl::OptionCategory MatcherSampleCategory("Matcher Sample");
+
+//std::string varName;
+bool counting = true;
+int count = 0;
+int choose;
+
+class VarDeclHandler : public MatchFinder::MatchCallback {
+public:
+    VarDeclHandler(Rewriter &Rewrite) : Rewrite(Rewrite) {}
+
+    virtual void run(const MatchFinder::MatchResult &Result) {
+      const BinaryOperator *bo = Result.Nodes.getNodeAs<clang::BinaryOperator>("bo");
+      
+      if(counting) {
+        count++;
+        return;
+      } else {
+        count++;
+        if(count != choose) {
+          return;
+        }
+      }
+
+      SourceLocation sl = bo->getSourceRange().getBegin();
+    
+      SourceLocation des = sl;
+      while (Lexer::findLocationAfterToken(des, tok::semi, Rewrite.getSourceMgr(), 
+                                    Rewrite.getLangOpts(), true).isInvalid()) {
+          //des = Lexer::getLocForEndOfToken(sl, 0, Rewrite.getSourceMgr(), Rewrite.getLangOpts());
+          des = des.getLocWithOffset(-1);
+      }
+      des = Lexer::findLocationAfterToken(des, tok::semi, Rewrite.getSourceMgr(), 
+                                    Rewrite.getLangOpts(), true); 
+      std::string str = "// Missing var assignment: ";
+
+      //std::cout << "fun: " <<str<< std::endl;
+
+      Rewrite.InsertTextAfter(des, str);
+    
+    }
+    
+private:
+    Rewriter &Rewrite;
+};
+
+// Implementation of the ASTConsumer interface for reading an AST produced
+// by the Clang parser. It registers a couple of matchers and runs them on
+// the AST.
+class MyASTConsumer : public ASTConsumer {
+public:
+    MyASTConsumer(Rewriter &R) : VD(R) {
+
+    
+        Matcher.addMatcher(binaryOperator(hasOperatorName("=")).bind("bo"),&VD);
+    }
+    
+    void HandleTranslationUnit(ASTContext &Context) override {
+        // Run the matchers when we have the whole TU parsed.
+        Matcher.matchAST(Context);
+    }
+    
+private:
+    MatchFinder Matcher;
+    VarDeclHandler VD;
+
+};
+
+// For each source file provided to the tool, a new FrontendAction is created.
+class MyFrontendAction : public ASTFrontendAction {
+public:
+    MyFrontendAction() {}
+    void EndSourceFileAction() override {
+        if(counting) return;
+        TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID())
+        .write(llvm::outs());
+    }
+    
+    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                   StringRef file) override {
+        TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+        return llvm::make_unique<MyASTConsumer>(TheRewriter);
+    }
+    
+private:
+    Rewriter TheRewriter;
+};
+
+int main(int argc, const char **argv) {
+    /*
+    std::cout << "Enter variable name: " << std::endl;
+    std::getline (std::cin,varName);
+    */
+    CommonOptionsParser op(argc, argv, MatcherSampleCategory);
+    ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+    Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
+    
+    counting = false;
+    srand(time(NULL));
+    choose = rand() % count + 1;
+    count = 0;
+
+    return Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
+}
